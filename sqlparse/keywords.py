6912bb56060784fce9a2591cf311c0a83dd670ff
liuzhenwu <liuzhenwu@mafengwo.com>
2018-11-07 10:22:34 +0800

    only use mysql, so remove oracle & pg keyword

--- sqlparse/keywords.py
+++ sqlparse/keywords.py
@@ -14,8 +14,8 @@ from sqlparse import tokens
 def is_keyword(value):
     val = value.upper()
     return (KEYWORDS_COMMON.get(val) or
-            KEYWORDS_ORACLE.get(val) or
-            KEYWORDS_PLPGSQL.get(val) or
+            # KEYWORDS_ORACLE.get(val) or
+            # KEYWORDS_PLPGSQL.get(val) or
             KEYWORDS.get(val, tokens.Name)), value
 
 
